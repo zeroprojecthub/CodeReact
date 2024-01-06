c21b8c37cfe1ba5efba6ed7cd3b9688d82de1df5
zeroprojecthub <143165786+zeroprojecthub@users.noreply.github.com>
2024-01-06 06:42:06 +0000

    first demo projects

---
+++ 01demoappnpx/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+
+    <h2>This is react npx demo</h2>
+
+  );
+}
+
+export default App;
---
+++ 01demoviteapp/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <h2>Thi is vite app</h2>
+    </>
+  )
+}
+
+export default App
